a85c51c369598b9a6e1b8750b632f60edf0f031e
Gannicus91 <max99giv@mail.ru>
2019-06-21 14:07:18 +0400

    добавил verbose_name к полям модели

--- apache_logs/models.py
+++ apache_logs/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class ApacheLog(models.Model):
-    ip = models.GenericIPAddressField()
-    date = models.DateTimeField()
-    tz = models.SmallIntegerField()
-    method = models.CharField(max_length=40)
-    referer = models.URLField(blank=True, null=True)
-    status = models.PositiveSmallIntegerField()
-    resp_size = models.PositiveIntegerField(blank=True, null=True)
+    ip = models.GenericIPAddressField(verbose_name='IP')
+    date = models.DateTimeField(verbose_name='Дата')
+    tz = models.SmallIntegerField(verbose_name='Часовой пояс')
+    method = models.CharField(max_length=40, verbose_name='HTTP метод')
+    referer = models.URLField(blank=True, null=True, verbose_name='Источник')
+    status = models.PositiveSmallIntegerField(verbose_name='Статус ответа')
+    resp_size = models.PositiveIntegerField(blank=True, null=True, verbose_name='Размер ответа')
 
     def __str__(self):
         return str(self.date)
