bc6d900ebf40dfe261cad150500f169481c876f7
Gannicus91 <max99giv@mail.ru>
2019-06-26 18:26:40 +0400

    Update get_logs.py

--- apache_logs/management/commands/get_logs.py
+++ apache_logs/management/commands/get_logs.py
@@ -30,35 +30,36 @@ class Command(BaseCommand):
             """
             obj_list = []
             wrote = 0
-            cut = ['', '']
+            cut = ['', ''] # первый элемент - часть строки считанная в прошлой итерации, второй - в этой
             for data in tqdm(response.iter_content(chunk_size=block_size),
                              total=math.ceil(total_size // block_size),
                              unit='KB', unit_scale=True, desc="Downloading & processing the data"):
                 data = data.decode('utf-8').split('\n')
-                cut[1] = data.pop()
-                stuck = cut[0] + data.pop(0)
+                cut[1] = data.pop() # запоминаем обрезанную строку для следующей итерации
+                stuck = cut[0] + data.pop(0) # склеиваем обрезанные части строк
                 if re.fullmatch(Command.pattern, cut[0]):
                     obj_list.append(Command.get_log_object(cut[0]))
                 else:
-                    if re.fullmatch(Command.pattern, stuck):
+                    if stuck:
                         obj_list.append(Command.get_log_object(stuck))
                 Command.process(data, obj_list)
-                cut[0] = cut[1]
+                cut[0] = cut[1] # обновляем cut для след. итерации
                 wrote += len(data)
                 Command.save_data(obj_list)
                 obj_list = []
             if total_size != 0 and wrote != total_size:
-                return 0
-            return 1
+                print("Something went wrong")
+                return False
+            return True
         except Exception as e:
             print(e)
-            return 0
+            return False
 
     @staticmethod
     def process(data, obj_list):
         for log in data:
             log_obj = Command.get_log_object(log)
-            if log_obj is None:
+            if log_obj:
                 continue
             else:
                 obj_list.append(log_obj)
@@ -67,7 +68,7 @@ class Command(BaseCommand):
     def get_log_object(log):
         match = re.search(Command.pattern, log)  # получили match-объект с интересующими группами
         if match is None:
-            return None
+            return False
         ip = match[1]
         date_time = datetime.strptime(match[2], "%d/%b/%Y:%H:%M:%S")
         tz = int(match[3][:3])
